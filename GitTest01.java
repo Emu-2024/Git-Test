bc55feade66572ef733d09085492e8f8f35537b4
Ermias <awethaile2019@gmail.com>
2024-05-16 18:05:38 -0400

    SECOND COMMIT

---
+++ GitTest01.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GitTest01 {
+
+	public static Scanner scan;
+
+	
+	
+	
+	public static void main(String[] args) {
+		reverseString();
+	}
+	
+	public static void reverseString() {
+		scan = new Scanner(System.in);
+		
+		System.out.println("Enter A NAME");
+
+		String name = scan.nextLine();
+
+		String reverse = "";
+
+		int length = name.length();
+
+		for (int i = length - 1; i >= 0; i--) {
+
+			reverse += name.charAt(i);
+
+		}
+
+		System.out.println("The Reverse is ===>" + reverse);
+
+	}
+
+}
